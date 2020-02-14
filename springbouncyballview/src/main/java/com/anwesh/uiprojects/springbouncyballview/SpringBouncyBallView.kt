850c250b8dc2d6a5e56227060f37934518a70cc0
Anwesh43 <anweshthecool0@gmail.com>
2020-02-14 09:54:44 +0530

    adding constatns and libraries

--- springbouncyballview/src/main/java/com/anwesh/uiprojects/springbouncyballview/SpringBouncyBallView.kt
+++ springbouncyballview/src/main/java/com/anwesh/uiprojects/springbouncyballview/SpringBouncyBallView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.springbouncyballview
 /**
  * Created by anweshmishra on 14/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")

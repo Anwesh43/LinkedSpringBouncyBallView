b20b6fe71be93e89ee0c355e9c3bb05afe7a2c4b
Anwesh43 <anweshthecool0@gmail.com>
2020-02-14 10:11:01 +0530

    adding Canvas extension method

--- springbouncyballview/src/main/java/com/anwesh/uiprojects/springbouncyballview/SpringBouncyBallView.kt
+++ springbouncyballview/src/main/java/com/anwesh/uiprojects/springbouncyballview/SpringBouncyBallView.kt
@@ -16,11 +16,57 @@ val nodes : Int = 5
 val parts : Int = 2
 val scGap : Float = 0.02f / parts
 val strokeFactor : Int = 90
-val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val springSizeFactor : Float = 7f
+val springXFactor : Float = 5f
+val ballSizeFactor : Float = 10f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSpringMotion(springSize : Float, springCurrX : Float, paint : Paint) {
+    save()
+    drawLine(0f, 0f, springCurrX, 0f, paint)
+    translate(springCurrX, 0F)
+    drawLine(0f, 0f, 0f, -springSize, paint)
+    restore()
+}
+
+fun Canvas.drawBallMotion(ballCurrX : Float, ballR : Float, springX : Float, paint : Paint) {
+    save()
+    translate(springX, 0f)
+    drawCircle(ballCurrX, 0f, ballR, paint)
+    restore()
+}
+
+fun Canvas.drawBouncySpringBall(w : Float, scale : Float, paint : Paint) {
+    val springSize : Float = w / springSizeFactor
+    val springX : Float = w / springXFactor
+    val ballR : Float = w / ballSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val ballX : Float = w - springX - 2 * ballR
+    val springCurrX : Float = springX * sf1
+    val ballCurrX : Float = ballX * sf2
+    save()
+    drawSpringMotion(springSize, springCurrX, paint)
+    drawBallMotion(ballCurrX, ballR, springX, paint)
+    restore()
+}
+
+fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawBouncySpringBall(w, scale, paint)
+    restore()
+}
